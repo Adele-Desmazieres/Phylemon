cac8a66a33eeef8ff80a9b466ff697c48edf3216
LAM Louise <lam16louise@hotmail.fr>
2022-05-12 17:23:16 +0200

    retirer les commentaires inutiles

--- fusion/src/AR2/Vue/Accueil.java
+++ fusion/src/AR2/Vue/Accueil.java
@@ -33,16 +33,10 @@ public class Accueil extends JPanel{
 
         // Création des auteurs
         JPanel createur0 = new JPanel();
-        //JPanel crea1 = new JPanel();
         JLabel createurs1 = new JLabel("Auteurs : Dounia B., Alexandre B., Linéa D., Benoit D., Adèle D., Joachim E., Ewen G., Louise L., Elvire T.");
-        //crea1.add(createurs1);
-        //JPanel crea2 = new JPanel ();
-        //JLabel createurs2 = new JLabel("Joachim EKOKA, Ewen GLASZIOU, Louise LAM, Elvire TRAMA");
-        //crea2.add(createurs2);
+        
         createur0.add(createurs1);
-        //createur0.add(createurs2);
         this.add(createur0, BorderLayout.SOUTH);
-        //this.add(createurs2, BorderLayout.SOUTH);
 
         this.add(new JPanel(),BorderLayout.WEST);
         this.add(new JPanel(), EAST);
